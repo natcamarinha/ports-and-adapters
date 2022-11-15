b6d41b23e5c14a5bb90360c74d3a9d8119262cc8
Natalia Camarinha <natcamarinha@gmail.com>
2022-11-15 19:38:28 -0300

    fix app and main

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,50 +1,9 @@
 <script setup lang="ts">
-  import { reactive, ref } from 'vue';
-
-  const todos: any = reactive([
-    {
-      description: 'Estudar Typescript',
-      done: true,
-    },
-    {
-      description: 'Fazer a prova online',
-      done: false,
-    },
-    {
-      description: 'Cortar a grama',
-      done: false,
-    }
-  ]);
-  const description = ref("");
-
-  function addItem() {
-    if (!description.value) return;
-    if (todos.some((item: any) => item.description === description.value)) return;
-    if (todos.filter((item: any) => !item.done).length > 4) return;
-    todos.push({ description: description.value, done: false });
-    description.value = '';
-  }
-
-  function removeItem(item: any) {
-    todos.splice(todos.indexOf(item), 1);
-  }
-
-  function toggleDone(item: any) {
-    item.done = !item.done;
-  }
+import TodoListView from './components/TodoListView.vue'
 </script>
 
 <template>
-  <div v-if="todos.length === 0">No Item</div>
-  <div v-for="item in todos">
-    <span :style="{ 'text-decoration': (item.done) ? 'line-through': ''}">
-      {{ item.description}}
-    </span>
-    <button @click="toggleDone(item)">Done/Undone</button>
-    <button @click="removeItem(item)">x</button>
-  </div>
-  {{ description }}
-  <input type="text" v-model="description" v-on:keyup.enter="addItem()"/>
+  <TodoListView></TodoListView>
 </template>
 
 <style scoped>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import AxiosAdapter from './infra/AxiosAdapter';
+import TodoHttpGateway from './gateways/TodoHttpGateway';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+const httpClient = new AxiosAdapter();
+const todoGateway = new TodoHttpGateway(httpClient, 'http://localhost:3000');
+app.provide('todoGateway', todoGateway);
+app.mount('#app');
