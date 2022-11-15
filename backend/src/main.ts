65329435393f346c21d7ebecf369d0112559b644
Natalia Camarinha <natcamarinha@gmail.com>
2022-11-15 18:52:50 -0300

    fix backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,21 +7,46 @@ app.use(cors());
 
 const todos = [
   {
+    id: Math.random().toString(36).slice(2, 7),
     description: 'Estudar Typescript',
     done: true,
   },
   {
+    id: Math.random().toString(36).slice(2, 7),
     description: 'Fazer a prova online',
     done: false,
   },
   {
+    id: Math.random().toString(36).slice(2, 7),
     description: 'Cortar a grama',
     done: false,
   }
 ];
 
 app.get('/todos', (req, res) => {
+  console.log('GET /todos');
   res.json(todos);
+});
+
+app.post('/todos', (req, res) => {
+  todos.push(req.body);
+  res.end()
+});
+
+app.delete('/todos/:id', (req, res) => {
+  const todo = todos.find((todo: any) => todo.id === req.params.id);
+  if (todo) {
+    todos.splice(todos.indexOf(todo), 1);
+  }
+  res.end()
+});
+
+app.put('/todos/:id', (req, res) => {
+  const todo = todos.find((todo: any) => todo.id === req.params.id);
+  if (todo) {
+    todo.done = req.body.done;
+  }
+  res.end()
 });
 
 app.listen(3000);
